c37a838d5836ed8dbfc2fd33f3ced5190b39d55d
42tg <tobiasgraf87@gmail.com>
2020-01-27 17:16:19 +0100

    add happy case

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import Button from "./components/Buttons";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 import defaultTheme from "./defaultTheme";
 
-import { createShallow } from "@material-ui/core/test-utils";
+import { createShallow, createMount } from "@material-ui/core/test-utils";
 
 describe("<Button />", () => {
   let shallow;
@@ -19,6 +19,24 @@ describe("<Button />", () => {
   });
   it("Should render the button", () => {
     const wrapper = shallow(<Button>Hello World</Button>);
+    expect(wrapper.container).toMatchSnapshot();
+  });
+});
+
+describe("<Button />", () => {
+  let mount;
+  beforeAll(() => {
+    mount = createMount({
+      wrappingComponent: ({ children, theme }) => (
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      ),
+      wrappingComponentProps: {
+        theme: createMuiTheme(defaultTheme)
+      }
+    });
+  });
+  it("Should render the button", () => {
+    const wrapper = mount(<Button>Hello World</Button>);
     expect(wrapper.container).toMatchSnapshot();
   });
 });
